fcb78b2221df5cd0da830e1d433065decfb6b14f
Sayfuddin Naeem <sayfuddinnaeem.cse4.bu@gmail.com>
2025-12-30 23:47:14 +0600

    feat: create getUserByFirebaseUid for getUserById controller

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,4 +1,5 @@
 const User = require("../models/user.model");
+const assertOwnership = require("../utils/ownershipGuard");
 
 const registerUser = async (userData) => {
   const existingUser = await User.findOne({
@@ -14,6 +15,21 @@ const registerUser = async (userData) => {
   return { user, created: true };
 };
 
+const getUserByFirebaseUid = async (firebaseUid, requester) => {
+  const user = await User.findOne({ firebaseUid });
+
+  assertOwnership({
+    resource: user,
+    ownerField: "_id",
+    requesterId: requester._id,
+    requesterRole: requester.role,
+    allowAdmin: false,
+  });
+
+  return user;
+};
+
 module.exports = {
   registerUser,
+  getUserByFirebaseUid,
 };
