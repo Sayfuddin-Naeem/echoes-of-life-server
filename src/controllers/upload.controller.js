08c95f935867fc6d89214f1400ff30b29f5209c2
Sayfuddin Naeem <sayfuddinnaeem.cse4.bu@gmail.com>
2025-12-30 16:04:09 +0600

    feat: create upload image controller

---
+++ src/controllers/upload.controller.js
@@ -0,0 +1,22 @@
+const cloudinary = require("../config/cloudinary");
+const { uploadImageToCloudinary } = require("../services/cloudinary.service");
+const AppError = require("../utils/AppError");
+
+// upload image to cloudinary
+exports.uploadImage = async (req, res, next) => {
+  try {
+    if (!req.file) {
+      throw new AppError("No file uploaded", 400);
+    }
+
+    const image = await uploadImageToCloudinary(req.file.buffer);
+
+    res.status(200).json({
+      success: true,
+      message: "Image uploaded successfully",
+      data: image,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
